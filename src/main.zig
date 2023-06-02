4f488dc4ebbf73b08864fbbaa2cf90614c0c3454
Sezoka <vlad.novport@gmail.com>
2023-06-03 00:47:50 +0300

    day 2 - opening an SDL2 window

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,69 @@
 const std = @import("std");
+const sdl = @import("sdl.zig");
 
-pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+fn handle_event(event: *sdl.SDL_Event) bool {
+    var should_quit = false;
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    switch (event.type) {
+        sdl.SDL_QUIT => {
+            std.debug.print("SDL_QUIT\n", .{});
+            should_quit = true;
+        },
+        sdl.SDL_WINDOWEVENT => {
+            switch (event.window.event) {
+                sdl.SDL_WINDOWEVENT_RESIZED => {
+                    std.debug.print("SDL_WINDOWEVENT_RESIZED {d} {d}\n", .{ event.window.data1, event.window.data2 });
+                    is_white = !is_white;
+                },
+                else => {},
+            }
+        },
+        else => {},
+    }
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // don't forget to flush!
+    return should_quit;
 }
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+var is_white = false;
+
+pub fn main() !void {
+    if (sdl.SDL_Init(sdl.SDL_INIT_VIDEO) != 0) {
+        sdl.SDL_Log("Unable to initialize SDL: %s", sdl.SDL_GetError());
+        return error.SDLInitializationFailed;
+    }
+    defer sdl.SDL_Quit();
+
+    const window = sdl.SDL_CreateWindow(
+        "Handmade Hero",
+        sdl.SDL_WINDOWPOS_UNDEFINED,
+        sdl.SDL_WINDOWPOS_UNDEFINED,
+        640,
+        480,
+        sdl.SDL_WINDOW_RESIZABLE,
+    ) orelse {
+        sdl.SDL_Log("Unable to create window: %s", sdl.SDL_GetError());
+        return error.SDLInitializationFailed;
+    };
+    defer sdl.SDL_DestroyWindow(window);
+
+    const renderer = sdl.SDL_CreateRenderer(window, -1, 0);
+    defer sdl.SDL_DestroyRenderer(renderer);
+
+    while (true) {
+        var event: sdl.SDL_Event = undefined;
+        _ = sdl.SDL_WaitEvent(&event);
+        if (handle_event(&event)) {
+            break;
+        }
+
+        _ = sdl.SDL_RenderClear(renderer);
+
+        if (is_white) {
+            _ = sdl.SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+        } else {
+            _ = sdl.SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        }
+
+        _ = sdl.SDL_RenderPresent(renderer);
+    }
 }
